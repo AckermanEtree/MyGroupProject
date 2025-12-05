f326534b95c39adc9c8df265da56727892239e51
wlz <wulz000@outlook.com>
2025-12-06 00:23:11 +0800

    Add subtract function by me

--- calculator.c
+++ calculator.c
@@ -4,6 +4,11 @@ int add(int a, int b) {
     return a + b;
 }
 
+// 我自己写的减法函数（函数名不同）
+int subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
     printf("Simple Calculator Project\n");
     printf("Version: 1.0\n");
